34e8f86ef968fccf90f20b0a32573582039eadec
microjumper <microjmpr+developer@gmail.com>
2021-03-18 15:29:04 +0100

    basic movement + camera flollow

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,50 +4,22 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    private readonly float speed = 720;
-    private readonly float rightAngle = 45;
-    private readonly float leftAngle = -45;
-
-    private bool isFacingRight;
-    private bool isTurning;
+    private readonly float speed = 2;
 
     // Start is called before the first frame update
     void Start()
     {
-        transform.rotation = Quaternion.Euler(0, rightAngle, 0);
-        isFacingRight = true;
-        isTurning = false;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = transform.position + transform.forward * speed * Time.deltaTime;
     }
 
     public void Turn()
     {
-        if(!isTurning)
-        {
-            StartCoroutine(SmoothTurn());
-        }
-    }
-
-    private IEnumerator SmoothTurn()
-    {
-        isTurning = true;
-
-        float targetAngle = isFacingRight ? leftAngle : rightAngle;
-        Quaternion targetRotation = Quaternion.Euler(0, targetAngle, 0);
-
-        while (transform.rotation != targetRotation)
-        {
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, speed * Time.deltaTime);
-            yield return null;
-        }
-
-        isFacingRight = !isFacingRight;
-
-        isTurning = false;
+        transform.rotation = Quaternion.Euler(0, -transform.rotation.eulerAngles.y, 0);
     }
 }
