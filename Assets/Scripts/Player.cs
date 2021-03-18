1c0fdc32d6cf6d8392ce2ec1ab2562c662ecc5c2
microjumper <microjmpr+developer@gmail.com>
2021-03-18 12:30:49 +0100

    player directions

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private readonly float speed = 720;
+    private readonly float rightAngle = 45;
+    private readonly float leftAngle = -45;
+
+    private bool isFacingRight;
+    private bool isTurning;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.rotation = Quaternion.Euler(0, rightAngle, 0);
+        isFacingRight = true;
+        isTurning = false;
     }
 
     // Update is called once per frame
@@ -16,9 +25,29 @@ public class Player : MonoBehaviour
         
     }
 
-    public void SwapDirection()
+    public void Turn()
     {
-        float currentAngle = transform.rotation.eulerAngles.y;
-        transform.rotation = Quaternion.Euler(0, -currentAngle, 0);
+        if(!isTurning)
+        {
+            StartCoroutine(SmoothTurn());
+        }
+    }
+
+    private IEnumerator SmoothTurn()
+    {
+        isTurning = true;
+
+        float targetAngle = isFacingRight ? leftAngle : rightAngle;
+        Quaternion targetRotation = Quaternion.Euler(0, targetAngle, 0);
+
+        while (transform.rotation != targetRotation)
+        {
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, speed * Time.deltaTime);
+            yield return null;
+        }
+
+        isFacingRight = !isFacingRight;
+
+        isTurning = false;
     }
 }
